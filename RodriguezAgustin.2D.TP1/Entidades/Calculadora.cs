a8ab73736e1f6cd583a879685a778557d721b999
AgustinRguez <agus_rodriguez-@hotmail.com>
2022-04-15 16:55:42 -0300

    Resubiendo tp1

---
+++ RodriguezAgustin.2D.TP1/Entidades/Calculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+		private static char ValidarOperador(char operador)
+		{
+			if (operador == '+' || operador == '-' || operador == '*' || operador == '/')
+				return operador;
+			else
+				return '+';
+		}
+
+		public static double Operar(Operando num1, Operando num2, char operador)
+		{
+			switch (ValidarOperador(operador))
+			{
+				case '+':
+					return num1 + num2;
+				case '-':
+					return num1 - num2;
+				case '*':
+					return num1 * num2;
+				case '/':
+					return num1 / num2;
+				default:
+					return 0;
+			}
+		}
+	}
+}
